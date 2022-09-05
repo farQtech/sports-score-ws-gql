fc7803f223d0f31fb38cd7e9d4e9d5c42bb9f204
Ikramuddin Farooqui <mfarooqui@azure.uworld.com>
2022-09-05 17:25:08 +0530

    updated tests

--- sport-score-monitoring.service/jest.config.ts
+++ sport-score-monitoring.service/jest.config.ts
@@ -2,7 +2,13 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    setupFiles: [
+        "./__tests__/dotenv-config.js"
+    ],
+    modulePathIgnorePatterns:['./dist'],
+    testPathIgnorePatterns: ['./__tests__/dotenv-config.js']
+    
 };
 
 export default config;
